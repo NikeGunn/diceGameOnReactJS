a8c8cc823750227b1c5fc05ff504b19e5ad1244f
Nikhil Bhagat <knewboy.nykhil@gmail.com>
2022-11-10 22:30:45 +0545

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,156 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@chakra-ui/button";
+import { Image } from "@chakra-ui/image";
+import { ListItem } from "@chakra-ui/layout";
+import { Link } from "@chakra-ui/layout";
+import { List } from "@chakra-ui/layout";
+import { Text } from "@chakra-ui/layout";
+import { Box, Flex, Heading, Stack } from "@chakra-ui/layout";
+import { background } from "@chakra-ui/react";
 
-function App() {
+import { useState } from "react";
+const App = () => {
+  const [gameStarted, setGameStarted] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
+  const [dice, setDice] = useState(1);
+  const [error, setError] = useState(null);
+  const [score, setScore] = useState(0);
+  const numbers = [1, 2, 3, 4, 5, 6];
+
+  const startGameHandler = () => {
+    setGameStarted(true);
+  };
+
+  const onNumberClicked = (value) => {
+    setSelectedNumber(value);
+    setError(null);
+  };
+
+  const genRandomNo = () => {
+    if (selectedNumber) {
+      const genratedNo = Math.ceil(Math.random() * 6);
+      setDice(genratedNo);
+
+      if (selectedNumber === genratedNo) {
+        setScore((prev) => prev + genratedNo);
+      } else {
+        setScore((prev) => prev - 2);
+      }
+    } else {
+      setError("Please Select Number");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {gameStarted ? (
+        <>
+          <Stack
+            justify="center"
+            align="center"
+            maxW="1300px"
+            mx="auto"
+            h="100vh"
+          >
+            <Heading
+              as="h1"
+              color={error ? "red" : "black"}
+              fontSize="6xl"
+              mb="8"
+            >
+              {error ? error : "Select Number"}
+            </Heading>
+            <Flex pb="10">
+              {numbers.map((value) => (
+                <Flex
+                  justify="center"
+                  align="center"
+                  h="50px"
+                  w="50px"
+                  bg={selectedNumber === value ? "green" : "black"}
+                  color="white"
+                  fontSize="2xl"
+                  key={value}
+                  mr={4}
+                  borderRadius="md"
+                  onClick={() => onNumberClicked(value)}
+                >
+                  {value}
+                </Flex>
+              ))}
+            </Flex>
+            <Box h="150px" width="150px" onClick={genRandomNo}>
+              <Image src={`/dice/dice${dice}.png`} />
+            </Box>
+
+            <Text as="p" fontSize="xl">
+              Click on dice to roll
+            </Text>
+
+            <Text
+              color={score > 0 ? "green" : "red"}
+              fontSize="8xl"
+              fontWeight="bold"
+            >
+              {score}
+            </Text>
+            <Text fontSize="6xl" fontWeight="bold">
+              Total Score
+            </Text>
+            <Button colorScheme='blue' coloonClick={() => setScore(0)}>Reset Score</Button>
+          </Stack>
+          <Stack maxW="900px" mx="auto">
+            <Heading as="h2">Game Rules:-</Heading>
+            <List>
+              <ListItem>Select any number</ListItem>
+              <ListItem>Click on dice to roll it</ListItem>
+              <ListItem>
+                Select number is equal to obtained dice result then you will get
+                same point of dice
+              </ListItem>
+              <ListItem>
+                if You are Wrong Score will be deducted by 2 points
+              </ListItem>
+              <p>YouTube Chennel | CodeWithNikhil |</p>
+              <ListItem>
+                <Link href="https://www.youtube.com/c/CodeWithNikhil/featured">
+                  Join YouTube channel
+                </Link>
+              </ListItem>
+              <ListItem>
+                <p>Facebook | Nikhil Bhagat |</p> 
+                <Link href="https://www.facebook.com/knewboy.nykhil/">
+                  Facebook
+                </Link>
+
+              </ListItem>
+              <ListItem>
+              &copy; CodeWithNikhil
+              </ListItem>
+            </List>
+          </Stack>
+        </>
+      ) : (
+        <Flex justify="center" align="center">
+          <Image width="50%" src="/dices.png" />
+          <Stack>
+            <Heading fontSize="7xl" as="h1">
+              {" "}
+              The Dice Game
+            </Heading>
+            <Button
+              alignSelf="flex-end"
+              bg="black"
+              color="white"
+              _hover={{ bg: "grey" }}
+              onClick={startGameHandler}
+            >
+              Start Game
+            </Button>
+          </Stack>
+        </Flex>
+      )}
+    </>
   );
-}
+};
 
 export default App;
